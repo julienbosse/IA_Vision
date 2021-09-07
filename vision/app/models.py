73c7d73244cc8f853ccc45dcba2ea7796fecead8
Julien B <julien@bosse.pro>
2021-09-07 13:55:40 +0200

    page inversion

--- vision/app/models.py
+++ vision/app/models.py
@@ -26,3 +26,35 @@ def gen_frames_detection():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+def gen_frames_inversion():
+    camera = cv.VideoCapture(0)
+    face_cascade = cv.CascadeClassifier('app/static/haarcascade_frontalface_default.xml')
+    while True:
+        success, frame = camera.read()
+        gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 8)
+        if not success:
+            break
+        else:
+            try:
+                x1,y1,w1,h1 = faces[0]
+                x2,y2,w2,h2 = faces[1]
+
+                face1 = frame[y1:y1+h1, x1:x1+w1]
+                face2 = frame[y2:y2+h2, x2:x2+w2]
+
+                face1 = cv.resize(face1, (w2,h2), interpolation= cv.INTER_LINEAR)
+                face2 = cv.resize(face2, (w1,h1), interpolation= cv.INTER_LINEAR)
+
+                frame[y1:y1+h1, x1:x1+w1] = face2
+                frame[y2:y2+h2, x2:x2+w2] = face1
+                
+            except:
+                pass
+
+            ret, buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
--- vision/app/views.py
+++ vision/app/views.py
@@ -6,6 +6,14 @@ from app import models
 def index():
     return render_template('index.html')
 
+@app.route('/inversion', methods=['GET', 'POST'])
+def inversion():
+    return render_template('inversion.html')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(models.gen_frames_detection(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/inversion_feed')
+def inversion_feed():
+    return Response(models.gen_frames_inversion(), mimetype='multipart/x-mixed-replace; boundary=frame')
